f698955586055df5a0cb259d18335ed46274524e
caio braga <caiobraga.dev@outlook.com>
2022-10-03 14:23:36 -0300

    Criação de novas classes

---
+++ Pedidos/Pedidos/Domain/Products/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Pedidos.Domain.Products
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Category Category { get; set; }
+
+    }
+}
